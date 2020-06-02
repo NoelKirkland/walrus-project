9940debba2bfeda79de8920eb6aba4678c19c9fa
Noel Kirkland <noelkirkland@gmail.com>
2020-06-02 14:58:24 -0700

    add make P tags clickable. Note function does not quite work yet
    co-authored-by: duverseaujohnny21 <duverseaujohnny21@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,27 @@ $(document).ready(function() {
     $("#walrus-showing").slideToggle();
     $("#walrus-hidden").slideToggle();
   });
-});
-$(document).ready(function() {
+
   $(".clickable2").click(function() {
     $(".seefirst").slideToggle();
     $(".seesecond").slideToggle();
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+  
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("p").click(function() {
+    $("p").removeClass("unhighlight");
+    $("p").addClass("highlight");
+  });
+  $("p").click(function() {
+    $("p").removeClass("highlight");
+    $("p").addClass("unhighlight");
   });
 });
